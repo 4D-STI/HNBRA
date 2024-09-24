0462e007c377366b35660ceaf3586123c7d1e5ac
devHB <devhb@outlook.com.br>
2024-09-24 18:20:48 -0300

    restart front-intranet

---
+++ front-end/00_hnbra-intranet/app/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <div id="header_container">
+            <h1 className="bg-blue-500 text-yellow-500">
+                Testando o componente recem criado !!!
+            </h1>
+        </div>
+    )
+}
