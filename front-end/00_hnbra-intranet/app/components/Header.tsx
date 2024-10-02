f9965beb528dabe8ed8882d396047205077bae00
manoel <manoelbsn112@gmail.com>
2024-10-02 11:43:47 -0300

    Adiciona components shadcn

--- front-end/00_hnbra-intranet/app/components/Header.tsx
+++ front-end/00_hnbra-intranet/app/components/Header.tsx
@@ -1,11 +1,53 @@
-
+import Image from "next/image"
+import Logo from "@/app/assets/logo_hnbra.png"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 
 export default function Header() {
     return (
-        <div id="header_container">
-            <h1 className="bg-blue-500 text-yellow-500">
-                Testando o componente recem criado !!!
-            </h1>
+       <header id="header" className="w-full bg-slate-50 shadow-sm">
+        <div id="container" className="w-screen">
+            <div id="logo.hnbra" className="">
+                <Image
+                src={Logo}
+                width={40}
+                height={60}>
+                </Image>
+                 <span className="br-divider vertical mx-half mx-sm1"></span>
+                <div className="">
+                    <h1>Marinha do Brasil</h1>
+                    <div>
+                        <ul>
+                           <li>Orgãos do governo</li>
+                           <li>Acesso à informação</li>
+                           <li>Legislação</li>
+                           <li>Acessibilidade</li>
+                           <Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+       </header>
     )
 }
