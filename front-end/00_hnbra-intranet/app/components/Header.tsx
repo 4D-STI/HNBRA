f1197140343f4a927913467e1d6945817e6e792f
manoel <manoelbsn112@gmail.com>
2024-10-02 18:06:32 -0300

    reestrutura componente login

--- front-end/00_hnbra-intranet/app/components/Header.tsx
+++ front-end/00_hnbra-intranet/app/components/Header.tsx
@@ -1,9 +1,9 @@
+import Login from "@/app/components/Login"
 import Image from "next/image"
 import Logo from "@/app/assets/logo_hnbra.png"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -14,54 +14,29 @@ import {
   import { Input } from "@/components/ui/input"
 
 export default function Header() {
+  
     return (
        <header id="header" className="w-full bg-slate-50 shadow-sm">
-        <div id="container" className="w-screen p-16 space-x-2">
+        <div id="container" className="max-w-screen p-16 space-x-2">
             <div id="logo.hnbra" className="">
                 <Image
                 src = {Logo}
+                alt="Logo"
                 width = {40}
                 height = {60}>
                 </Image>
                  <span className="br-divider vertical mx-half mx-sm1"></span>
                 <div className="">
                     <h1>Marinha do Brasil</h1>
-                    <div>
+                    <div className="flex space-x-4">
                         <ul>
                            <li>Orgãos do governo</li>
                            <li>Acesso à informação</li>
                            <li>Legislação</li>
                            <li>Acessibilidade</li>
                            <div id="button_login">
-                           <Dialog>
-                     <DialogTrigger asChild>
-        <Button variant="outline">Login</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Login</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Usuário
-            </Label>
-            <Input type="usuario" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="usuario" className="text-right">
-              Senha
-            </Label>
-            <Input type="senha" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Login</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-    </div>
-
+                            <Login/>
+                          </div>
                         </ul>
                     </div>
                 </div>
---
+++ front-end/00_hnbra-intranet/app/components/Login.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Login</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Usuário
+              </Label>
+              <Input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="password" className="text-right">
+                Senha
+              </Label>
+              <Input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Login</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+
+   
