b4da03695a892d9f36cc6bcb6a42849d80b32b8c
manoel <manoelbsn112@gmail.com>
2024-10-03 17:41:35 -0300

    import/ Importação fontes

--- front-end/00_hnbra-intranet/app/layout.tsx
+++ front-end/00_hnbra-intranet/app/layout.tsx
@@ -3,14 +3,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/app/components/Footer"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const Rawline = localFont({
+  src: "./fonts/rawline-300.ttf",
+  variable: "--font-rawline",
   weight: "100 900",
 });
 
@@ -28,7 +23,7 @@ export default function RootLayout({
     
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${Rawline.variable} ${Rawline.variable} antialiased`}
       >
         {children}
       </body>
