87d1c8c8e6ee52e9e9e6eb0cca9d7809654ed36b
devHB <devhb@outlook.com.br>
2024-10-04 17:45:16 -0300

    docker done

---
+++ 00_hnbra-intranet/app/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <div id="header_container">
+            <h1 className="bg-blue-500 text-yellow-500">
+                O projeto foi enjaulado !!!
+            </h1>
+        </div>
+    )
+}
