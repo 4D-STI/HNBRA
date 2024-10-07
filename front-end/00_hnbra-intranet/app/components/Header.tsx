e3d6774fb12235fbbcf434d984541b7f06e68a80
manoel <manoelbsn112@gmail.com>
2024-10-07 16:29:09 -0300

    UPDATE/adiciona novos elementos no Header

--- front-end/00_hnbra-intranet/app/components/Header.tsx
+++ front-end/00_hnbra-intranet/app/components/Header.tsx
@@ -1,12 +1,4 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-import { Home, Settings, User } from 'lucide-react';
+import { ContrastIcon } from 'lucide-react';
 import Login from "@/app/components/Login";
 import Image from "next/image";
 import Slidebar from "./Slidebar";
@@ -15,34 +7,34 @@ import Search from "@/app/components/Search"
 
 export default function Header() {
   return (
-    <header className="bg-gray-50 shadow-sm">
-      <div id="container" className="container max-w-screen-lg mx-auto p-4">
-      <div id="container-top" className="flex flex-col md:flex-row items-center justify-between">
-            <div id="container-logo-titulo"className="flex items-center"><Image src={Logo} alt="Logo" width={40} height={60} />
-            <span className="vertical mx-2"></span>
-            <h1 className="text-base">Marinha do Brasil</h1>
+    <header className="shadow-sm text-blue-900 bg-blue-50">
+      <div id="container" className="container max-w-screen-xl mx-auto p-2">
+      <div id="container-top" className="flex justify-between items-center p-2">
+            <div id="container-logo-titulo"className="flex items-center">
+              <Image src={Logo} alt="Logo" className="w-10 h-16"/>
+            <h1 className="text-xl px-4">Marinha do Brasil</h1>
             </div>
-            <div>
-            <ul className="flex flex-wrap items-center justify-end mx-16">
-              <li className="mr-4 text-sm">Orgãos do governo</li>
-              <li className="mr-4 text-sm">Acesso à informação</li>
-              <li className="mr-4 text-sm">Legislação</li>
-              <li className="mr-4 text-sm">Acessibilidade</li>
+            <div className="sm: hidden md:flex text-lg">
+            <ul className="flex  items-center justify-end mx-16">
+              <li className="mr-4"><a>Orgãos do governo</a></li>
+              <li className="mr-4"><a>Acesso à informação</a></li>
+              <li className="mr-4"><a>Legislação</a></li>
+              <li className="mr-4"><a>Acessibilidade</a></li>
             </ul>
-             </div> 
-              <div className="divider w-0.5 bg-slate-800"></div>
-              <div id="button_login" className="flex items-end mx-4">
+             </div>
+             <div id="dark-mode" className="flex p-2 hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer"><ContrastIcon className=""></ContrastIcon></div>
+              <div id="button_login" className="flex items-center mx-4">
                 <Login />
           </div>
         </div>
-        <div id="container-low" className="flex md:flex-row items-center p-2 ">
-        <div id="container-menu">
-          <Slidebar/>
+        <div id="container-low" className="flex md:flex-row items-center p-2">
+        <div id="container-menu" className='flex p-2 hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer'>
+          <Slidebar></Slidebar>
         </div>
   
-         <div id="container-titulo-2" className="flex flex-col px-1">
-      <h2 className="font-bold">HNBRA</h2>
-     <p className="font-normal">Hospital Naval De Brasília</p>
+         <div id="container-titulo-2" className="flex flex-col px-4">
+      <h2 className=" text-xl font-bold">HNBRA</h2>
+     <p className="text-x1">Hospital Naval De Brasília</p>
    </div>
   
      <div className="flex items-end ml-auto"> 
