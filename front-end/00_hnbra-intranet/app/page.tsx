c1f24e0a067121adc998c3737bdbc435fd8ff4ab
manoel <manoelbsn112@gmail.com>
2024-10-09 15:16:54 -0300

    Add/  icone acesso rápido

--- front-end/00_hnbra-intranet/app/page.tsx
+++ front-end/00_hnbra-intranet/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import { Grip } from 'lucide-react';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hope</h1>
+    <div id="container-buttons-pages" className=" flex max-w-screen-lg p-4 items-center mx-auto">
+      <div id="container-button-acesso" className="flex p-2 hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer">
+          <Grip/>
+      </div>
+      <div id="acesso-rapido-titlle">
+        <p>Acesso rápido</p>
+      </div>
     </div>
      
   );
