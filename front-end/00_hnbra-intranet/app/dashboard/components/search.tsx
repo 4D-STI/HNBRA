02049419e485c37152a9335d583c8aa0670f9172
manoel <manoelbsn112@gmail.com>
2024-10-15 16:27:30 -0300

    UPDATE/lista de usuários e componente de busca

--- front-end/00_hnbra-intranet/app/dashboard/components/search.tsx
+++ front-end/00_hnbra-intranet/app/dashboard/components/search.tsx
@@ -11,7 +11,7 @@ import {
    
   export default function CommandDemo() {
     return (
-      <Command className="rounded-lg border shadow-md md:min-w-[450px] bg-blue-200">
+      <Command className="rounded-lg border shadow-md md:min-w-[450px] bg-blue-50">
         <CommandInput placeholder="Pesquisar..." />
       </Command>
     )
--- front-end/00_hnbra-intranet/app/dashboard/gerenciarUsuarios/listUsers.tsx
+++ front-end/00_hnbra-intranet/app/dashboard/gerenciarUsuarios/listUsers.tsx
@@ -1,4 +1,4 @@
-import { Toggle } from "@/components/ui/toggle"
+import { Toggle } from "@/components/ui/toggle";
 import {
   Table,
   TableBody,
@@ -8,8 +8,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import Command from "./components/Command"
-import { Eye, Edit, Search, Trash2, Filter } from "lucide-react";
+import Command from "./components/Command";
+import { Eye, Plus, Download, Trash2, Filter } from "lucide-react";
 
 interface User {
   nome: string;
@@ -17,6 +17,7 @@ interface User {
   departamento: string;
   secao: string;
   status: string;
+  permissoes: string[];
 }
 
 const users: User[] = [
@@ -26,6 +27,7 @@ const users: User[] = [
     departamento: "Administração",
     secao: "Seção de Informática",
     status: "Ativo",
+    permissoes: ["read", "delete", "download"],
   },
   {
     nome: "Maria da Silva",
@@ -33,6 +35,7 @@ const users: User[] = [
     departamento: "Saúde",
     secao: "Serviço de medicina operativa",
     status: "Ativo",
+    permissoes: ["read", "add"],
   },
   {
     nome: "João Oliveira",
@@ -40,6 +43,7 @@ const users: User[] = [
     departamento: "Diretoria",
     secao: "Conselho Técnico",
     status: "Inativo",
+    permissoes: ["read", "delete"],
   },
   {
     nome: "Ana Clara",
@@ -47,51 +51,72 @@ const users: User[] = [
     departamento: "Administração",
     secao: "Seção de esportes",
     status: "Ativo",
+    permissoes: ["read", "add", "download", "delete"],
   },
 ];
 
 export default function ListUsers() {
   return (
     <div className="p-2">
-      <div className="flex items-center justify-center mb-4">
-        <div className="relative w-80">
-          <Command></Command>          
+      <div className="flex flex-col md:flex-row items-center justify-center mb-4">
+        <div className="relative w-full md:w-80">
+          <Command />
         </div>
-          <div className="flex px-4">
+        <div className="flex px-4 mt-2 md:mt-0">
           <Filter className="mr-1" />
           Filtrar
-          </div>
-       
+        </div>
       </div>
 
-      <Table className="text-base">
-        <TableHeader className="bg-blue-900">
-          <TableRow>
-            <TableHead className="text-gray-50 px-16">Nome</TableHead>
-            <TableHead className="text-gray-50 px-16">Nip</TableHead>
-            <TableHead className="text-gray-50 px-16">Departamento</TableHead>
-            <TableHead className="text-gray-50 px-16">Seção</TableHead>
-            <TableHead className="text-gray-50 px-16">Status</TableHead>
-            <TableHead className="text-gray-50 px-16">Ações</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {users.map((user, index) => (
-            <TableRow key={index}>
-              <TableCell className="px-16">{user.nome}</TableCell>
-              <TableCell className="px-16">{user.nip}</TableCell>
-              <TableCell className="px-16">{user.departamento}</TableCell>
-              <TableCell className="px-16">{user.secao}</TableCell>
-              <TableCell className="px-16">{user.status}</TableCell>
-              <TableCell className="px-16 flex">
-                <Toggle><Eye className="mr-1" /></Toggle>
-                <Toggle><Trash2 className="mr-1"/></Toggle>
-                <Toggle> <Edit className="mr-1" /></Toggle>
-              </TableCell>
+      <div className="overflow-x-auto">
+        <Table className="min-w-full">
+          <TableHeader className="bg-blue-900 text-gray-50">
+            <TableRow>
+              <TableHead className="text-gray-50 text-left w-1/5">Nome</TableHead>
+              <TableHead className="text-gray-50 text-left w-1/5">NIP</TableHead>
+              <TableHead className="text-gray-50 text-left w-1/5">Departamento</TableHead>
+              <TableHead className="text-gray-50 text-left w-1/5">Seção</TableHead>
+              <TableHead className="text-gray-50 text-left w-1/5">Status</TableHead>
+              <TableHead className="text-gray-50 text-left w-1/5">Ações</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {users.map((user, index) => (
+              <TableRow key={index} className="hover:bg-gray-100">
+                <TableCell className="px-4 text-left">{user.nome}</TableCell>
+                <TableCell className="px-4 text-left">{user.nip}</TableCell>
+                <TableCell className="px-4 text-left">{user.departamento}</TableCell>
+                <TableCell className="px-4 text-left">{user.secao}</TableCell>
+                <TableCell className="px-4 text-left">{user.status}</TableCell>
+                <TableCell className="px-4 text-left">
+                  <div className="flex space-x-4 justify-start">
+                    {user.permissoes.includes("read") && (
+                      <button>
+                        <Eye />
+                      </button>
+                    )}
+                    {user.permissoes.includes("delete") && (
+                      <button>
+                        <Trash2 />
+                      </button>
+                    )}
+                    {user.permissoes.includes("add") && (
+                      <button>
+                        <Plus />
+                      </button>
+                    )}
+                    {user.permissoes.includes("download") && (
+                      <button>
+                        <Download />
+                      </button>
+                    )}
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
