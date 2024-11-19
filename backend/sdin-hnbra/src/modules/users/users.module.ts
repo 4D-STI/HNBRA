666b316584eed8498e32c915e1503ee21521b852
breno <brenomarcio846@gmail.com>
2024-11-19 15:54:34 -0300

    configurando user

--- backend/sdin-hnbra/src/modules/users/users.module.ts
+++ backend/sdin-hnbra/src/modules/users/users.module.ts
@@ -4,6 +4,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { usersProviders } from './users.provider';
 import { DatabaseModule } from '../../bd/db.module';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [DatabaseModule],
@@ -11,6 +12,12 @@ import { DatabaseModule } from '../../bd/db.module';
   providers: [
     UsersService,
     ...usersProviders,
+    UsersRepository,
+  ],
+  exports: [
+    UsersService, // Exporte o UsersService para uso em outros módulos
+    ...usersProviders, // Exporte o USERS_REPOSITORY para uso em outros módulos
+    UsersRepository,
   ],
 })
 
