eafa362d5f9fcdb7114a5f2ba7e94bbde571deca
devHB <devhb@outlook.com.br>
2024-12-27 22:27:55 -0300

    CHECKPOINT: v0.0.1

---
+++ TODO.md
@@ -0,0 +1,30 @@
+<!-- banner -->
+Botões de navegação do banner
+
+<!-- header -->
+add link redemarinha
+link: https://redemarinha.intranet.mb/homepage/login/
+
+Sistemas SSM
+link: http://dsm.mb/sistemas-em-producao
+
+
+add link Bono Geral
+
+<!-- side bar -->
+remover link "Relação de Usuários Zimbra"
+
+<!-- buscador -->
+Bussola
+
+<!-- Funcionalidades -->
+
+Agendamento da sala de reuniões
+Agendamento do auditorio link:https://www.conforsup.mb/webforsup/agenda-auditorio
+
+<!-- Botão -->
+Seções?
+Programa Netuno
+
+<!-- Anuncios multimedia -->
+add carrosel
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,17 @@
 // import Image from "next/image";
 import { Grip } from 'lucide-react';
 // shadcn: navigation-menu
-import { NavigationMenuDemo } from '@/app/components/utils/shadcn_demo_components/navigation-menu'
+// import { NavigationMenuDemo } from '@/app/components/utils/shadcn_demo_components/navigation-menu'
 import { CarouselDemo } from './components/utils/shadcn_demo_components/carousel';
 // react
 import { CardForAdvertisement } from './components/utils/shadcn_demo_components/card_with_ad';
 import { CardForAdvertisementWithoutLink } from './components/utils/shadcn_demo_components/card_with_ad_without_link';
 import { CardWithMultiMedia } from './components/utils/shadcn_demo_components/card_with_multimedia';
 import { CardWithHighlights } from './components/utils/shadcn_demo_components/card_with_highlights';
-import ShortcutsOtherApps from '@/app/components/shortcuts/ShortcutsOtherApps'
-// import Link from 'next/link';
+// data
+import {pracaPadrao,servidorCivilPadrao} from '@/app/components/utils/shadcn_demo_components/data/card_with_highlights_data'
 
+import ShortcutsOtherApps from '@/app/components/shortcuts/ShortcutsOtherApps'
 
 export default function Home() {
 
@@ -23,9 +24,9 @@ export default function Home() {
     <>
 
       {/* C1 - atalhos, navegação, suporte */}
-      <div id="c-1-container" className="flex h-20 max-w-screen p-2 items-center justify-center mx-auto relative">
+      <div id="c-1-container" className="flex h-10 mt-2 max-w-screen items-center justify-center mx-auto relative">
 
-        <div id="container-shortcut-button" className="flex px-8 py-2 ml-32 items-center hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer absolute left-0">
+        <div id="container-shortcut-button" className="flex px-8 py-2 ml-12 items-center hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer absolute left-0">
 
           <div id="container-shortcut-icon" className="mr-4">
             <Grip />
@@ -36,7 +37,7 @@ export default function Home() {
           </div>
         </div>
 
-        <NavigationMenuDemo />
+        {/* <NavigationMenuDemo /> */}
 
         {/* <div id="container-userManager-link" className="py-2 px-8 hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer">
           <Link href="/dashboard/userManager">
@@ -54,7 +55,7 @@ export default function Home() {
 
 
       {/* C2 - banner, botoes atalhos */}
-      <div id='c-2-container' className='flex flex-row h-96 mt-2 items-center'>
+      <div id='c-2-container' className='flex flex-row h-96 items-center'>
 
         {/* container - botões atalhos */}
         <div id='files-container' className='grid grid-cols-9  w-1/2 content-stretch px-8 ml-8'>
@@ -68,29 +69,34 @@ export default function Home() {
       </div>
 
       {/* C3 - praça padrão, anuncios */}
-      <div id='c-3-container' className='flex flex-col flex-grow flex-shrink w-full h-2/6 mt-4'>
+      <div id='c-3-container' className='flex flex-col flex-grow flex-shrink w-full h-auto'>
 
         <div id='c-3-content-container' className='flex h-full'>
 
           {/* praças e servidor civil padrão */}
-          <div id="anuncios-multimidia" className="w-6/12 px-6 bg-green-100">
+          <div id="anuncios-multimidia" className="w-6/12 px-6">
 
-            <div id="multimedia-title-container" className='mb-2'>
+            <div id="multimedia-title-container" className='mb-2 font-bold'>
               Praças e Servidor Civil Padrão
             </div>
 
             <div id="multimedia-content-container" className="flex flex-row gap-4">
-              <CardWithHighlights/>
-              <CardWithHighlights/>
-              <CardWithHighlights/>
-              <CardWithHighlights/>
+              {/* praça */}
+              {pracaPadrao?.map((praca, i) => {
+                return <CardWithHighlights key={i} pracaPadrao={praca}/>
+              })}
+
+              {/* servidor civil padrão */}
+              {servidorCivilPadrao?.map((servidor, i) => {
+                return <CardWithHighlights key={i} servidorCivilPadrao={servidor} />
+              })}
             </div>
 
           </div>
           {/* anuncios multimidia */}
-          <div id="anuncios-multimidia" className="w-4/12 px-6 bg-red-100">
+          <div id="anuncios-multimidia" className="w-4/12 px-6">
             
-            <div id="multimedia-title-container" className='mb-2'>
+            <div id="multimedia-title-container" className='mb-2 font-bold'>
               Anuncios Multimidia
             </div>
 
@@ -102,9 +108,9 @@ export default function Home() {
           </div>
 
           {/* anuncios textos*/}
-          <div id="anuncios-texto" className="flex-col w-4/12 px-6 bg-yellow-100">
+          <div id="anuncios-texto" className="flex-col w-4/12 px-6">
             
-            <div id="advertisement-title-container" className="mb-2">
+            <div id="advertisement-title-container" className="mb-2 font-bold">
               <h1>Anúncios</h1>
             </div>
 
