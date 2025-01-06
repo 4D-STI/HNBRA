8144ba0bdef1807ce1b0f5fc039275490f94c1f6
devHB <devhb@outlook.com.br>
2025-01-06 10:54:42 -0300

    REFAT: one line footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,12 +1,14 @@
 export default function Footer() {
     return (
-        <footer id="footer-layout-default" className="flex fixed bottom-0 left-0 right-0 bg-blue-800 h-24 text-white content-center">
-            <address id="footer-text-container" className="flex-col not-italic text-sm my-2 ml-8">
+        <footer id="footer-layout-default" className="flex fixed bottom-0 left-0 right-0 bg-blue-800 h-auto text-white items-center justify-center">
+            <address id="footer-text-container" className="flex flex-row not-italic text-sm gap-8 p-2">
                 <h1 className="">Hospital Naval de Brasília</h1>
+                <p>|</p>
                 <p className="">SEPS Q 711/911 - Asa Sul, Brasília - DF, 73390-115</p>
+                <p>|</p>
                 <p className="">(61) 3445-7303</p>
-                <p className="">CNPJ: 00.394.502/0060-02
-                </p>
+                <p>|</p>
+                <p className="">CNPJ: 00.394.502/0060-02</p>
             </address>
         </footer>
              )
