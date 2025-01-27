28a236b47f61ce0bb43036ec7a259c55117d247a
devHB <devhb@outlook.com.br>
2025-01-27 11:52:43 -0300

    refat: desabilita reescrita de rota

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,24 @@
-/** @type {import('next').NextConfig} */
+    /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    async rewrites() {
-        return [
-            {
-                source: '/api/files/:id/download',
-                destination: `${process.env.NEXT_PUBLIC_API_BACK}/files/:id/download`,
-            },
-            {
-                source: '/api/files/:name/view',
-                destination: `${process.env.NEXT_PUBLIC_API_BACK}/files/:name/view`,
-            },
-        ];
-    },
+    // output: 'standalone',
+    // async rewrites() {
+    //     console.log('REGRAS: ', this.rewrites());
+        
+    //     return [
+    //         {
+    //             source: '/api/files/:id/download',
+    //             destination: `${process.env.NEXT_PUBLIC_API_BACK}/files/:id/download`,
+    //         },
+    //         {
+    //             source: '/api/files/:name/view',
+    //             destination: `${process.env.NEXT_PUBLIC_API_BACK}/files/:name/view`,
+    //         },
+    //         {
+    //             source: '/api/files/:id/viewLast',
+    //             destination: `${process.env.NEXT_PUBLIC_API_BACK}/files/:id/viewLast`,
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
