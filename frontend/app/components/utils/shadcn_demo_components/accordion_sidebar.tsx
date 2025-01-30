c3130ec9472db7541364852c80d132f995862d48
devHB <devhb@outlook.com.br>
2025-01-30 15:52:38 -0300

    REFAT: linter

--- frontend/app/components/utils/shadcn_demo_components/accordion_sidebar.tsx
+++ frontend/app/components/utils/shadcn_demo_components/accordion_sidebar.tsx
@@ -10,22 +10,34 @@ import * as sideBarLinks from "@/app/components/sidebar/utils/sidebar_data";
 export function AccordionSideBar() {
   return (
     <Accordion type="single" collapsible className="w-full">
+      
       {sideBarLinks.menuTitles.map((title, index) => (
+        
         <AccordionItem key={index} value={`item-${index}`}>
+          
           <AccordionTrigger>{title.name}</AccordionTrigger>
-          {title.ref.map((link, linkIndex) => (
-            <AccordionContent key={linkIndex}>
-              {/* TODO: Tratamento de erro caso arquivo indisponível */}
-              <LinkSlideBar
-                key={linkIndex}
-                href={link.href}
-                text={link.name}
-                target="blank"
-              />
-            </AccordionContent>
-          ))}
+            
+            {title.ref.map((link, linkIndex) => (
+              
+              <AccordionContent key={linkIndex}>
+                
+                {/* TODO: Tratamento de erro caso arquivo indisponível */}
+                <LinkSlideBar
+                  key={linkIndex}
+                  href={link.href}
+                  text={link.name}
+                  target="blank"
+                  linkIndex={linkIndex}
+                />
+
+              </AccordionContent>
+
+            ))}
+
         </AccordionItem>
+        
       ))}
+
     </Accordion>
   );
 }
