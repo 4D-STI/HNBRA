34f31e2962ffc2602796258e17176bfc2a3a5494
devhb-b2b <devhb@outlook.com.br>
2025-02-06 17:14:51 -0300

    [HB] UPDATE: anuncios, swagger, homepage (#57)

--- backend/sdin-hnbra/src/modules/users/dto/create-user.dto.ts
+++ backend/sdin-hnbra/src/modules/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsEmpty, IsNotEmpty, IsString } from 'class-validator'
 
 const IS_NOT_EMPTY_MSG = (attr) => `O atributo ${attr} é obrigatório`
@@ -6,58 +6,104 @@ const IS_STRING_MSG = (attr) => `O atributo ${attr} deve ser uma string`
 const IS_EMPTY_MSG = (attr) => `O atributo ${attr} deve ser vazio`
 
 export class CreateUserDto {
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Número de Identicação Pessoal (NIP) do usuário.',
+        example: '12345678',
+        minLength: 8,
+        maxLength: 8
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('nip') })
     @IsString({ message: IS_STRING_MSG('nip') })
     nip: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'ID da patente do usuário.',
+        example: 1,
+        maxLength: 19
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('idPatent') })
     idPatent: number;
 
-    @ApiProperty({})
+    @ApiPropertyOptional({
+        description: 'Nome de guerra do usuário (opcional).',
+        example: 'Brito'
+    })
     @IsString({ message: IS_STRING_MSG('warName') })
     warName?: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Primeiro nome do usuário.',
+        example: 'Carlos'
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('firstName') })
     @IsString({ message: IS_STRING_MSG('firstName') })
     firstName: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Último nome do usuário.',
+        example: 'Daniel'
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('lastName') })
     @IsString({ message: IS_STRING_MSG('lastName') })
     lastName: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Cargo do usuário',
+        example: 'user',
+        enum: ['root', 'userAdm', 'contentAdm', 'user']
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('role') })
     @IsString({ message: IS_STRING_MSG('role') })
     role: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Status do cadastro do usuário.',
+        example: 'active',
+        enum: ['active', 'inactive', 'password_reset']
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('status') })
     @IsString({ message: IS_STRING_MSG('status') })
     status: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Permissões do usuário.',
+        example: 'write, read, delete',
+        examples: {
+            write: 'Permite criar ou atualizar informações.',
+            read: 'Permite a leitura de informações.',
+            delete: 'Permite deletar informações.'
+        }
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('permission') })
     @IsString({ message: IS_STRING_MSG('permission') })
     permission: string;
 
-    @ApiProperty({})
+    @ApiProperty({
+        description: 'Senha do usuário.',
+        example: '@HNBra1234'
+    })
     @IsNotEmpty({ message: IS_NOT_EMPTY_MSG('password') })
     @IsString({ message: IS_STRING_MSG('password') })
     password: string;
 
-    @ApiProperty({})
+    @ApiPropertyOptional({
+        description: 'Email pessoal do usuário (opcional).',
+        example: 'usuario@email.com'
+    })
     @IsString({ message: IS_STRING_MSG('emailPersonal') })
     emailPersonal?: string;
 
-    @ApiProperty({})
+    @ApiPropertyOptional({
+        description: 'Email de Marinha do usuário (opcional).',
+        example: 'usuario@marinha.milbr'
+    })
     @IsString({ message: IS_STRING_MSG('emailMb') })
     emailMb?: string;
 
-    @ApiProperty({})
+    @ApiPropertyOptional({
+        description: 'Número de contato do usuário (opcional).',
+        example: '61988776655'
+    })
     @IsString({ message: IS_STRING_MSG('contactNumber') })
     contactNumber?: string;
 }
