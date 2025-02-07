9321eb119b7882a6f57c86adfdaa4d5c0577b948
devHB <devhb@outlook.com.br>
2025-02-07 10:54:51 -0300

    REFAT: file rename

---
+++ frontend/app/components/utils/validations/password_validation.tsx
@@ -0,0 +1,30 @@
+export default function PasswordValidation(pass: string): string[] {
+  const errors: string[] = [];
+
+  if (!/(?=.*[A-Z])/.test(pass)) {
+    errors.push("Senha inválida");
+    return errors; 
+  }
+
+  if (!/(?=.*\d)/.test(pass)) {
+    errors.push("Senha inválida");
+    return errors; 
+  }
+
+  if (!/(?=.*[!@#$%^&()_+{}\[\]:;<>,.?\/\\-])/.test(pass)) {
+    errors.push("Senha inválida");
+    return errors; 
+  }
+
+  if (/(.)\1{2}/.test(pass)) {
+    errors.push("Senha inválida");
+    return errors; 
+  }
+
+  if (pass.length < 6 || pass.length > 8) {
+    errors.push("Senha inválida");
+    return errors; 
+  }
+
+  return errors;
+}
