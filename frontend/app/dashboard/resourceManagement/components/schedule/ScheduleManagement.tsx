5965888d8d6c66e5993c1f9f66cd17cd22978feb
devHB <devhb@outlook.com.br>
2025-02-20 16:13:32 -0300

    REAFT: rearrange dir and create component ScheduleManagement

--- frontend/app/dashboard/resourceManagement/components/schedule/ScheduleManagement.tsx
+++ frontend/app/dashboard/resourceManagement/components/schedule/ScheduleManagement.tsx
@@ -3,76 +3,75 @@
 import React, { useCallback, useEffect, useState } from "react"
 import {CalendarPlus, RefreshCw} from 'lucide-react'
 import ScheduleForm from "./ScheduleForm"
+import ScheduleItemContent from "./ScheduleItemContent"
+import { ScheduleType, IScheduleItem, OrderType } from "./schedule_interface/scheduleInterface"
+import * as filters from "./utils/filters"
 
-const url ='http://localhost:3002/scheduling/schedulingTrue'
-
-interface IScheduleItem {
-idScheduling: number;
-nip: string;
-schedulingStart: Date;
-schedulingEnd: Date;
-nameResponsible: string;
-theme: string;
-description: string;
-typeScheduling: ScheduleType;
-ramal: string;
-status: boolean;
-createdAt: Date;
-updatedAt: Date;
-}
-
-enum ScheduleType {
-    auditorium = 'Auditório',
-    meeting_room = 'Sala de Reuniões'
-}
-
-// metodo para formatar data
-const handlerDateFormat = (date: Date) => {
-    return new Date(date).toLocaleString('pt-br', {day: 'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric'})
-}
+const url ='http://localhost:3002/scheduling'
 
 export const ScheduleManagement = () => {
     // gerencia o tipo dos agendamentos listados
     const handleScheduleTypeButtonToggle = (event: React.MouseEvent<HTMLButtonElement>) => {
         const target = event.target as HTMLButtonElement
-        if (target.value === 'auditorium') {
+        if (target.value === ScheduleType.auditorium) {
             setCreateSchedule(false)
-            setMeetingRoomSelected(false)
-            setAuditoriumSelected(!isAuditoriumSelected)
+            setContentType(target.value)
+            setScheduleType(target.value)
         }
 
-        if (target.value === 'meeting-room') {
+        if (target.value === ScheduleType.meeting_room) {
             setCreateSchedule(false)
-            setMeetingRoomSelected(!isMeetingRoomSelected)
-            setAuditoriumSelected(false)
+            setContentType(target.value)
+            setScheduleType(target.value)
         }
     }
 
     // gerencia o conteudo (criar ou listar)
     const handleScheduleContent = () => {
         setCreateSchedule(!createSchedule)
-        setAuditoriumSelected(false)
-        setMeetingRoomSelected(false)
+        setContentType(null)
+        setScheduleType(null)
     }
 
-    // gerencia atualização dos dados
-    // const handleReload = () => setReload(!reload)
+    // gerencia ordenação do conteúdo
+    const handlerOrder = () => {
+        
+        if (order === 'asc') {
+            setOrder(OrderType.des)
+            return
+        }
+        
+        if (order === 'des') {
+            setOrder(OrderType.asc)
+            return
+        }
+    }
 
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(true)
     const [reload, setReload] = useState(false)
     const [data, setData] = useState<IScheduleItem[]>([])
-    const [isAuditoriumSelected,setAuditoriumSelected] = useState(false)
-    const [isMeetingRoomSelected,setMeetingRoomSelected] = useState(false)
+    const [contentType,setContentType] = useState<ScheduleType | null>(null)
     const [createSchedule, setCreateSchedule] = useState(true)
+    const [order, setOrder] = useState(OrderType.asc)
+    const [search_term, setSearch_Term] = useState('')
+    const [scheduleType, setScheduleType] = useState<ScheduleType | null>(null)
+    const [param_for_filter, setParam_For_Filter] = useState<keyof IScheduleItem>('schedulingStart')
 
-
+    // filtros de processamento dos dados
+    const filteredData = filters.filteredData({data, order, param_for_filter, scheduleType, search_term})
+    
     const fetchData = useCallback(async () => {
+        console.log('entrou no fetchdata');
+        
         setReload(true)
+        
         if (!url) {
             setError("Erro: Url inexistente!");
             console.log(error);
             console.log(loading);
+            console.log(setSearch_Term);
+            
             
             setLoading(false);
             return;
@@ -83,20 +82,23 @@ export const ScheduleManagement = () => {
                 const res = await fetch(url, { cache: "no-store" });
                 if (!res.ok) throw new Error("Erro ao buscar conteúdo.");
                 const data = await res.json();
-                await setData(data);
+                // setData(filters.filteredData({data, order, param_for_filter, scheduleType, search_term}));
+                setData(data);
                 setReload(false)
-            }, 2000);
+            }, 1000);
         } catch (err) {
             setError(err instanceof Error ? err.message : "Erro desconhecido");
         } finally {
             setLoading(false);
         }
-    }, []);
+    }, []); // estado derivado <<<
 
     // Executa a busca ao carregar
     useEffect(() => {
+        console.log('entrou em effect');
+                
         fetchData();
-    },[fetchData]);
+    },[]); // estado derivado <<<
 
     return (
         <div
@@ -122,7 +124,7 @@ export const ScheduleManagement = () => {
                     <button
                         className={`
                             rounded-2xl p-2 text-white font-bold text-sm
-                            ${isAuditoriumSelected ? "bg-blue-600" : "bg-gray-600"}
+                            ${contentType === 'auditorium' ? "bg-blue-600" : "bg-gray-600"}
                         `}
                         onClick={(e) => handleScheduleTypeButtonToggle(e)}
                         value='auditorium'
@@ -133,7 +135,7 @@ export const ScheduleManagement = () => {
                     <button
                         className={`
                             rounded-2xl p-2 text-white font-bold text-sm
-                            ${isMeetingRoomSelected ? "bg-blue-600" : "bg-gray-600"}
+                            ${contentType === 'meeting-room' ? "bg-blue-600" : "bg-gray-600"}
                         `}
                         onClick={(e) => handleScheduleTypeButtonToggle(e)}
                         value='meeting-room'
@@ -143,7 +145,17 @@ export const ScheduleManagement = () => {
                 </div>
 
                 {/* botoes direita */}
-                <div>
+                <div className="flex flex-row gap-4">
+                    {/* ordenação */}
+                    <button
+                        className="flex flex-row bg-green-500 rounded-2xl 2-auto px-4 items-center"
+                        onClick={handlerOrder}
+                    >
+                        <p className="mr-2">{"Filtro ID: "}</p>
+                        <p>{order.toLocaleUpperCase()}</p>
+                    </button>
+
+                    {/* atualizar conteúdo */}
                     <button
                         className={`
                             flex gap-2 rounded-2xl p-2 text-white font-bold text-sm
@@ -161,51 +173,26 @@ export const ScheduleManagement = () => {
             {/* container do conteúdo dos agendamentos */}
             <div id="schedule-content-container" className="border-2 border-black mt-2 h-full w-full rounded-2xl p-2 overflow-hidden">
 
-                    <div
-                        id="content-title-container"
-                        className="font-bold"
-                    >
-                        {isAuditoriumSelected && 'Agendamentos do Auditório'}
-                        {isMeetingRoomSelected && 'Agendamentos da Sala de Reuniões'}
-                        {createSchedule && 'Formulário de Novo Agendamento'}
-                    </div>
+                <div
+                    id="content-title-container"
+                    className="font-bold"
+                >
+                    {(contentType === 'auditorium') && 'Agendamentos do Auditório'}
+                    {(contentType === 'meeting-room') && 'Agendamentos da Sala de Reuniões'}
+                    {createSchedule && 'Formulário de Novo Agendamento'}
+                </div>
                 
 
-               {/* LISTA DOS AGENDAMENTOS */}
-               {
-                (isAuditoriumSelected || isMeetingRoomSelected) &&
-                (<div className="overflow-y-auto h-full mt-2 pb-8">
-                    {data?.sort((a,b) => b.idScheduling - a.idScheduling).map((item, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className="mt-4 ml-4 mr-4"
-                        >
-                            <div
-                                key={item.idScheduling}
-                                className="ml-4"
-                            >
-                                <p>id: {item.idScheduling}</p>
-                                <p>tipo: {item.typeScheduling}</p>
-                                <p>criação: {handlerDateFormat(item.createdAt)}</p>
-                                <p>descrição: {item.description}</p>
-                                <p>responsavel: {item.nameResponsible}</p>
-                                <p>nip: {item.nip}</p>
-                                <p>ramal: {item.ramal}</p>
-                                <p>inicio do agendamento: {handlerDateFormat(item.schedulingStart)}</p>
-                                <p>fim do agendamento: {handlerDateFormat(item.schedulingEnd)}</p>
-                            </div>
-    
-                            <div
-                                key={index}
-                                className="border-2 border-black mt-4"
-                            >
-                            </div>
-                        </div>
-                        )
-                    })}
-
-                </div>)
+                {/* LISTA DOS AGENDAMENTOS */}
+                {
+                    contentType &&
+                    <div className="overflow-y-auto h-full mt-2 pb-8">
+                        {
+                            <ScheduleItemContent 
+                                data={filteredData}
+                            />
+                        }
+                    </div>
                 }
 
                 {/* FORMULARIO DE CRIAÇÃO DE AGENDAMENTO */}
@@ -213,7 +200,7 @@ export const ScheduleManagement = () => {
                     createSchedule &&
                     (
                         <div className="overflow-y-auto h-full mt-2 pl-8 pr-20 pb-12">
-                            <ScheduleForm/>
+                            <ScheduleForm />
                         </div>
                     )
                 }
--- frontend/app/dashboard/resourceManagement/components/users/ScheduleManagement.tsx
+++
@@ -1,9 +0,0 @@
-export const UsersManagement = () => {
-    return (
-        <div>
-            {'módulo em construção'.toUpperCase()}
-        </div>
-    )
-}
-
-export default UsersManagement;
