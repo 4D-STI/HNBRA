d1eb511ebc8e2915925e3090bfcfa8a63612dfe5
devHB <devhb@outlook.com.br>
2025-02-24 16:22:54 -0300

    DELETE: Componente fixo de anúncios e componente inutilizado de menu;

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -50,162 +50,126 @@ export default function Home() {
   }, []);
 
 
-  // const information: InformationType[] = await response.json();
-
-  // const ANUNCIO_TI = "Os chamados para suporte de informática deverão ser acompanhados pelos respectivos solicitantes. A cada alteração no ciclo de atendimento do chamado, o sistema - de forma automática - envia um e-mail ao requisitante que, poderá conter informações fundamentais para a resolução do problema relatado."
-
-  // const ANUNCIO_ADMINISTRACAO = "As vagas de estacionamento do HNBra são de uso exclusivo das viaturas administrativas da MB e da tripulação do HNBra, distribuídas conforme a OI 20-32A, exceto para as situações previstas na legislação (idosos por exemplo). Para acesso, o veículo deverá obrigatoriamente portar o cartão de estacionamento ou de utilização de vaga exclusiva."
-
-  // const ANUNCIO_SAUDE = "O agendamento de atendimento do PME (Programa de Medicamentos Especiais) será realizado pelo telefone 3445-7340 - RETELMA 8916 (Farmácia)."
-
   return (
     <PDF_DataProvider>
-      <>
-        {/* C1 */}
-        <div id="c-1-container" className="flex flex-row py-1 max-w-screen items-center space-x-4 bg-blue-200 justify-between">
+        <>
+          {/* C1 */}
+          <div id="c-1-container" className="flex flex-row py-1 max-w-screen items-center space-x-4 bg-blue-200 justify-between">
 
-          <div id='menu-principal-container' className='ml-20'>
-            {/* slide bar */}
-            <Slidebar />
-          </div>
-
-          <div id='nav-button-container' className='flex flex-row'>
-            {/* shortcut button */}
-            {/* <ShortcutButton/> */}
-
-
-            {/* bono geral */}
-            <BonoGeralButton />
-
-            {/* bono sede */}
-            <BonoSedeButton />
-
-            {/* clinicas credenciadas */}
-            <ClinicasCredenciadasButton />
-
-            {/* plano do dia */}
-            <PlanoDoDiaPage />
-
-            {/* cardapio */}
-            <CardapioButton />
-          </div>
-
-          <div id="busca-container" className=''>
-            {/* busca */}
-            <Search />
-          </div>
-
-
-
-          {/* <NavigationMenuDemo /> */}
-
-          {/* <div id="container-userManager-link" className="py-2 px-8 hover:bg-blue-300 transition duration-200 rounded-full cursor-pointer">
-            <Link href="/dashboard/userManager">
-              <p>Gerenciamento de Usuários</p>
-            </Link>
-          </div>
-
-          <div id="container-userManager-link" className="py-2 px-8 hover:bg-blue-300 transition duration\-200 rounded-full cursor-pointer">
-            <Link href="/dashboard/filesManagement">
-              <p>Gerenciamento de Arquivos</p>
-            </Link>
-          </div> */}
-
-        </div>
-
-
-        {/* C2 - banner, botoes atalhos */}
-        <div id='c-2-container' className='flex flex-row h-96 items-center'>
-
-          {/* container - botões atalhos */}
-          <div id='files-container' className='grid grid-cols-9  w-1/2 content-stretch px-8 ml-8'>
-            <ShortcutsOtherApps />
-          </div>
-
-          {/* container - banner carousel */}
-          <div id='carousel-container' className='flex flex-row w-1/2 h-[350px] rounded-xl container mx-auto mr-6'>
-            <SwiperCarousel />
-          </div>
-        </div>
-
-        {/* C3 - praça padrão, anuncios */}
-        <div id='c-3-container' className='flex flex-col flex-grow flex-shrink w-full h-auto'>
-
-          <div id='c-3-content-container' className='flex h-full'>
-
-            {/* praças e servidor civil padrão */}
-            <div id="praca-sc-padrao-container" className="w-6/12 px-6">
-
-              <div id="praca-sc-padrao-title-container" className='mb-2 font-bold'>
-                Praças e SC Padrão
-              </div>
-
-              <div id="multimedia-content-container" className="flex flex-row gap-4">
-                {/* praça */}
-                {pracaPadrao?.map((praca, i) => {
-                  return <CardWithHighlights key={i} pracaPadrao={praca} />
-                })}
-
-                {/* servidor civil padrão */}
-                {servidorCivilPadrao?.map((servidor, i) => {
-                  return <CardWithHighlights key={i} servidorCivilPadrao={servidor} />
-                })}
-              </div>
-
-            </div>
-            {/* anuncios multimidia */}
-            <div id="anuncios-multimidia" className="w-2/12 px-6">
-
-              <div id="multimedia-title-container" className='mb-2 font-bold'>
-                Multimídia
-              </div>
-
-              <div id="multimedia-content-container" className="">
-                {/* todo: fazer carrosel */}
-                <CardWithMultiMedia />
-              </div>
-
+            <div id='menu-principal-container' className='ml-20'>
+              {/* slide bar */}
+              <Slidebar />
             </div>
 
-            {/* anuncios textos*/}
-            <div id="anuncios-texto" className="flex-col w-4/12 px-6 h-96 overflow-y-scroll">
+            <div id='nav-button-container' className='flex flex-row'>
+              {/* shortcut button */}
+              {/* <ShortcutButton/> */}
 
-              <div id="advertisement-title-container" className="mb-2 font-bold flex items-center justify-between">
-                <h1>Informações Gerais</h1>
-                {isLogin && (
-                  <Link href={"/dashboard/filesManagement"}>
-                    <AddIcon />
-                  </Link>
-                )}
+
+              {/* bono geral */}
+              <BonoGeralButton />
+
+              {/* bono sede */}
+              <BonoSedeButton />
+
+              {/* clinicas credenciadas */}
+              <ClinicasCredenciadasButton />
+
+              {/* plano do dia */}
+              <PlanoDoDiaPage />
+
+              {/* cardapio */}
+              <CardapioButton />
+            </div>
+
+            <div id="busca-container" className=''>
+              {/* busca */}
+              <Search />
+            </div>
+
+          </div>
+
+
+          {/* C2 - banner, botoes atalhos */}
+          <div id='c-2-container' className='flex flex-row h-96 items-center'>
+
+            {/* container - botões atalhos */}
+            <div id='files-container' className='grid grid-cols-9  w-1/2 content-stretch px-8 ml-8'>
+              <ShortcutsOtherApps />
+            </div>
+
+            {/* container - banner carousel */}
+            <div id='carousel-container' className='flex flex-row w-1/2 h-[350px] rounded-xl container mx-auto mr-6'>
+              <SwiperCarousel />
+            </div>
+          </div>
+
+          {/* C3 - praça padrão, anuncios */}
+          <div id='c-3-container' className='flex flex-col flex-grow flex-shrink w-full h-auto'>
+
+            <div id='c-3-content-container' className='flex h-full'>
+
+              {/* praças e servidor civil padrão */}
+              <div id="praca-sc-padrao-container" className="w-6/12 px-6">
+
+                <div id="praca-sc-padrao-title-container" className='mb-2 font-bold'>
+                  Praças e SC Padrão
+                </div>
+
+                <div id="multimedia-content-container" className="flex flex-row gap-4">
+                  {/* praça */}
+                  {pracaPadrao?.map((praca, i) => {
+                    return <CardWithHighlights key={i} pracaPadrao={praca} />
+                  })}
+
+                  {/* servidor civil padrão */}
+                  {servidorCivilPadrao?.map((servidor, i) => {
+                    return <CardWithHighlights key={i} servidorCivilPadrao={servidor} />
+                  })}
+                </div>
+
               </div>
+              {/* anuncios multimidia */}
+              <div id="anuncios-multimidia" className="w-2/12 px-6">
 
-              <div id="anuncio-texto-container" className="flex flex-col gap-3">
+                <div id="multimedia-title-container" className='mb-2 font-bold'>
+                  Multimídia
+                </div>
 
-                {/* <CardForAdvertisement session='TI' content={ANUNCIO_TI} link='https://siscsrecim.ctim.mb'/> */}
-
-                {/* <CardForAdvertisementWithoutLink session='TI' content={ANUNCIO_TI} />
-
-                <CardForAdvertisementWithoutLink session='Administração' content={ANUNCIO_ADMINISTRACAO} />
-
-                <CardForAdvertisementWithoutLink session='Saúde' content={ANUNCIO_SAUDE} />
-
-                <CardForAdvertisementWithoutLink session='Saúaaaaaaaaade' content={ANUNCIO_SAUDE} /> */}
-
-                <div id="anuncio-texto-container" className="flex flex-col gap-3">
-                  {information.map((anuncio, i) => (
-                    <CardForAdvertisementWithoutLink key={i} session={anuncio.nameDepartament} content={anuncio.description} />
-                  ))}
+                <div id="multimedia-content-container" className="">
+                  {/* todo: fazer carrosel */}
+                  <CardWithMultiMedia />
                 </div>
 
               </div>
 
+              {/* anuncios textos*/}
+              <div id="anuncios-texto" className="flex-col w-4/12 px-6 h-96 overflow-y-scroll">
+
+                <div id="advertisement-title-container" className="mb-2 font-bold flex items-center justify-between">
+                  <h1>Informações Gerais</h1>
+                  {isLogin && (
+                    <Link href={"/dashboard/filesManagement"}>
+                      <AddIcon />
+                    </Link>
+                  )}
+                </div>
+
+                <div id="anuncio-texto-container" className="flex flex-col gap-3">
+
+                  <div id="anuncio-texto-container" className="flex flex-col gap-3">
+                    {information.map((anuncio, i) => (
+                      <CardForAdvertisementWithoutLink key={i} session={anuncio.nameDepartament} content={anuncio.description} />
+                    ))}
+                  </div>
+
+                </div>
+
+              </div >
             </div >
+
           </div >
-
-
-
-        </div >
-      </>
+        </>
     </PDF_DataProvider >
   );
 }
