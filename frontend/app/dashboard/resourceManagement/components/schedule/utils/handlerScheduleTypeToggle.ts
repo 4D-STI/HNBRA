9fdb86d4b3213aefc455957bbf16d1142f0e126f
devHB <devhb@outlook.com.br>
2025-02-24 16:40:46 -0300

    ADD: metodo para alterar a renderização dos agendamentos pelo tipo

---
+++ frontend/app/dashboard/resourceManagement/components/schedule/utils/handlerScheduleTypeToggle.ts
@@ -0,0 +1,27 @@
+'use client'
+
+interface IHandleScheduleTypeToggle {
+    event: React.MouseEvent<HTMLButtonElement>,
+    setCreateSchedule: React.Dispatch<React.SetStateAction<boolean>>,
+    setContentType: React.Dispatch<React.SetStateAction<ScheduleType | null>>,
+    setScheduleType: React.Dispatch<React.SetStateAction<ScheduleType | null>>
+}
+
+import React from "react"
+import { ScheduleType } from "../schedule_interface/scheduleInterface"
+
+export const handleScheduleTypeButtonToggle = ({ event, setContentType, setCreateSchedule, setScheduleType }: IHandleScheduleTypeToggle): void => {
+
+    const target = event.target as HTMLButtonElement
+    if (target.value === ScheduleType.auditorium) {
+        setCreateSchedule(false)
+        setContentType(target.value)
+        setScheduleType(target.value)
+    }
+
+    if (target.value === ScheduleType.meeting_room) {
+        setCreateSchedule(false)
+        setContentType(target.value)
+        setScheduleType(target.value)
+    }
+}
