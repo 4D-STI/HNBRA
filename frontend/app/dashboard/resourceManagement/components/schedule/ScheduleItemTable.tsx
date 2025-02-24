428784e9549a5d389af51a6215e5de3bfe03a59a
devHB <devhb@outlook.com.br>
2025-02-24 16:35:22 -0300

    ADD: componente de ações editar/deletar; REFAT: estilização das celulas; REFAT: segregação e formatação da informação do dia/mes do agendamento

--- frontend/app/dashboard/resourceManagement/components/schedule/ScheduleItemTable.tsx
+++ frontend/app/dashboard/resourceManagement/components/schedule/ScheduleItemTable.tsx
@@ -1,41 +1,55 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {IScheduleItem} from './schedule_interface/scheduleInterface'
+import {HandleEditDelete} from './HandleEditDelete'
+import { jwtDecode } from 'jwt-decode'
 
 interface IScheduleItemComponent {
     item: IScheduleItem,
 }
 
-// const handleFilter = () => {
-
-// }
-
 export const ScheduleItemTable = ({item}: IScheduleItemComponent) => {
     const [expandTheme, setExpandTheme] = useState(false)
     const [expandDescription, setExpandDescription] = useState(false)
+    const [nipDecoded, setNipDecoded] = useState<IScheduleItem | null>(null)
+    const [token, setToken] = useState('')
+
+    useEffect(() => {
+        const token = localStorage.getItem('token') ?? ''
+        setToken(token)
+        if (token.length !== 0) setNipDecoded(jwtDecode(token))
+    }, [])
     
     return (
         <tr className="border-b border-gray-200 hover:bg-gray-50 text-xs text-center text-nowrap">
             
 
+            {/* editar/excluir | condição ter o memso nip do agendamento*/}
+            <td>
+                {
+                    Number(nipDecoded?.nip) === Number(item.nip) && <HandleEditDelete scheduleId={item.idScheduling} token={token}/>
+                }
+            </td>
+            
             {/* data do agendamento */}
             <td className="px-4 py-2">
-                {new Date(item.schedulingStart).toLocaleString('pt-br', {day:'2-digit', month:'short'}).toUpperCase()}
+                {
+                    `${new Date(item.schedulingStart).toLocaleString('pt-br', {day:'2-digit'}).toUpperCase()}
+                    ${new Date(item.schedulingStart).toLocaleString('pt-br', {month:'short'}).toUpperCase().replace('.','')}`
+                }
             </td>
             {/* horario inicio/fim */}
-            <td className="px-4 py-2">
-                <div className="flex flex-row items-center">
-                    <div className="bg-green-200 px-2 py-1 rounded-xl mr-2">
+            <td className="flex flex-row px-4 py-2 justify-center">
+                    <td className="bg-green-200 px-2 py-1 rounded-xl mr-2">
                         {new Date(item.schedulingStart).toLocaleString('pt-br', {timeStyle:'short'})}h
-                    </div>
-                    <div className="bg-red-200 px-2 py-1 rounded-xl">
+                    </td>
+                    <td className="bg-red-200 px-2 py-1 rounded-xl">
                         {new Date(item.schedulingEnd).toLocaleString('pt-br', {timeStyle:'short'})}h
-                    </div>
-                </div>
+                    </td>
             </td>
             {/* nome do resposável */}
-            <td className="px-4 py-2">
+            <td className="px-4 py-2 text-start">
                 {item.nameResponsible}
             </td>
             {/* nip do usuario responsável */}
@@ -52,7 +66,7 @@ export const ScheduleItemTable = ({item}: IScheduleItemComponent) => {
             </td>
             {/* tema do agendamento */}
             <td 
-                className={`${expandTheme ? '' : 'truncate'} px-4 py-2 text-start`}
+                className={`${expandTheme ? '' : ''} truncate px-4 py-2 text-start w-[200px]`}
                 onClick={() => setExpandTheme(!expandTheme)}
                 title={item.theme}
             >
@@ -60,7 +74,7 @@ export const ScheduleItemTable = ({item}: IScheduleItemComponent) => {
             </td>
             {/* descrição do agendamento */}
             <td 
-                className={`${expandDescription ? '' : 'truncate'} px-4 py-2 text-start`}
+                className={`${expandDescription ? '' : ''} hover:truncate-none hover:text-wrap truncate px-4 py-2 text-start`}
                 onClick={() => setExpandDescription(!expandDescription)}
                 title={item.description}
             >
