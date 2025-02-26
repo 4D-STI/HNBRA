ac524a21fce2f83343de4ae718c3186a4e330e5d
devHB <devhb@outlook.com.br>
2025-02-26 18:43:10 -0300

    REFAT: remove importação de componente inexistente

--- frontend/app/dashboard/subSessionFileList/descriptions/descriptionsMap.tsx
+++ frontend/app/dashboard/subSessionFileList/descriptions/descriptionsMap.tsx
@@ -1,5 +1,4 @@
 import SFH_Description from './SFH_Description'
-import PlanoDiaPDDescription from './PlanoDiaPDDescription'
 import SEDIME_Description from './SEDIME_Description'
 import PME_Description from './PME_Description'
 
@@ -7,9 +6,6 @@ export const descriptionMap = (title: string) => {
     switch (title) {
         case 'SERVICO_DE_FARMACIA_HOSPITALAR':
             return <SFH_Description/>
-            break
-        case 'PLANO DIA - PD':
-            return <PlanoDiaPDDescription/>
         case 'SEDIME':
             return <SEDIME_Description/>
         case 'PROGRAMA DE MEDICAMENTOS ESPECIAIS':
