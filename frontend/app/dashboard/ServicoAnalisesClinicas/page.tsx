ea7e6937736e72ccc8947d7217836fda38f66142
devHB <devhb@outlook.com.br>
2025-02-26 18:29:45 -0300

    STYLE: altera margens

--- frontend/app/dashboard/ServicoAnalisesClinicas/page.tsx
+++ frontend/app/dashboard/ServicoAnalisesClinicas/page.tsx
@@ -2,26 +2,28 @@
 
 export default function ServicoAnalisesClinicas() {
   return (
-    <div className="max-w-6xl pt-4 px-12 bg-white shadow-lg rounded-lg" property="content:encoded">
-        <p id="sac-tittle" className="font-bold">Serviço de Análises Clínicas</p>
+    <div className="max-w-6xl pt-4 px-12 bg-white shadow-lg rounded-lg text-sm">
+        
+        <p id="sac-tittle" className="font-bold text-center text-base">Serviço de Análises Clínicas</p>
+        
         <span id="sac-content" className="flex flex-col gap-2 indent-8 text-justify">
           <p className="mt-4">
             O SAC do HNBra realiza exames de análises clínicas ambulatoriais, assim como exames de emergência, de inspeção de saúde e de pacientes internados. Atende demandas provenientes dos pacientes ambulatoriais, do SEP (Setor de Estabilização de Pacientes), do Centro Cirúrgico, da Unidade de Internação, da UTI e da Junta de Saúde.
           </p>
-          <p>
+          <p className="mb-2">
             O paciente ambulatorial deve comparecer no Serviço de Análises Clínicas para retirada de senha de atendimento das 07h às 09h, às segundas, terças, quintas ou sextas-feiras, portando identidade e pedido médico. Não há necessidade de agendamento. As quartas-feiras são reservadas exclusivamente para os exames referentes às inspeções de saúde.
           </p>
-          <p>Os casos de emergência são atendidos 24h, diariamente.</p>
-          <p>
+          <p className="mb-2">Os casos de emergência são atendidos 24h, diariamente.</p>
+          <p className="mb-2">
             Os resultados podem ser visualizados pela internet no sítio eletrônico: navalbrasilia.buscalaudos.com.br com a chave de acesso e senha fornecidos no protocolo do paciente após o atendimento ou entregues fisicamente no Serviço de Análises Clínicas.
           </p>
-          <p>
+          <p className="mb-2">
             A entrega de resultados, assim como a emissão de guias para realização de exames em Laboratórios Credenciados é realizada das 10h às 17h, de segunda a sexta-feira.
           </p>
-          <p>
+          <p className="mb-2">
             Fim cumprir ordem de prioridade determinada no inciso 6.2.9 da DGPM-401, 4ª Revisão, e diminuir custos com encaminhamento de exames de análises clínicas, participo que toda demanda de exames de análises clínicas será absorvida pelo SAC do HNBra até atingir capacidade de atendimento, sem comprometer a qualidade do serviço prestado e a demanda excedente será encaminhada inicialmente para Organizações Militares de Saúde de outra Força Armada ou Forças Auxiliares, através de Guia de Apresentação do Usuário (GAU). A demanda não absorvida por outras Forças Armadas ou Forças Auxiliares será então encaminhada para Organizações de Saúde Civis credenciadas pelo HNBra, também através de emissão de GAU, conforme manifestação expressa e escolha do paciente/responsável.
           </p>
-          <p className="mt-4">
+          <p className="mb-2">
             Cabe ressaltar ordem de prioridade de atendimento definidas por Lei:
           </p>
           <p>
@@ -33,7 +35,7 @@ export default function ServicoAnalisesClinicas() {
           <p>
             3) militar da ativa, quando uniformizado (inciso 6.2.7 da DGPM-401).
           </p>
-          <p className="mt-4 mb-4">
+          <p className="mt-2 mb-4">
             Funcionamento: de segunda a segunda-feira, 24h.  
           </p>
         </span>
