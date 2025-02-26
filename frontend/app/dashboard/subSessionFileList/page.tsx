3d436b63ce34fdd185aca7eed1b8e17e7f37b927
devHB <devhb@outlook.com.br>
2025-02-26 18:30:29 -0300

    FEAT: implementa componente de descrição da Sub Sessão.

--- frontend/app/dashboard/subSessionFileList/page.tsx
+++ frontend/app/dashboard/subSessionFileList/page.tsx
@@ -4,6 +4,8 @@ import React, { useCallback, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import FileList from "../listSubSessionSession/file";
 import { File } from "../../types/file";
+import { SubSessionDescription } from "./SubSessionDescription";
+import descriptionMap from "./descriptions/descriptionsMap";
 
 export default function ListPage() {
     //Lista SubSession a partir de nome ou id
@@ -50,18 +52,46 @@ export default function ListPage() {
     if (loading) return <div>Carregando...</div>;
     if (error) return <div>{error}</div>;
 
+    console.log('NOME SUB: ', SUBSESSION_NAME);
+    
+
     return (
-        <div id="div-list-file">
-            <h1 className="text-2xl font-bold mb-4">
-                Lista de Arquivos {(SUBSESSION_NAME ?? "").replace(/_/g, " ")}
-            </h1>
-            <button
-                className="text-blue-500 underline mb-4"
-                onClick={() => router.back()} // Navegar para a página anterior
-            >
-                voltar
-            </button>
-            <FileList files={files} />
+        <div id="div-list-file" className="flex flex-col">
+            
+            {/* cabeçalho */}
+            <div id="fileList-header-container" className="flex flex-row justify-center gap-8 items-center mb-6">
+
+                <h1 className="text-2xl font-bold mb-4">
+                    Lista de Arquivos {(SUBSESSION_NAME ?? "").replace(/_/g, " ")}
+                </h1>
+
+                <button
+                    className="text-blue-600 hover:underline mb-4 p-2 bg-white w-20 rounded-2xl text-nowrap"
+                    onClick={() => router.back()} // Navegar para a página anterior
+                >
+                    {'voltar'}
+                </button>
+            </div>
+
+            {/* conteudo */}
+            <div id="content-container" className="flex flex-row gap-8 h-[600px]">
+
+                {/* Descrição */}
+                { (descriptionMap(SUBSESSION_NAME ?? '') ) && (
+                    <div className="flex w-1/2 px-8 h-auto justify-center">
+                        <SubSessionDescription title={SUBSESSION_NAME ?? ''}/>
+                    </div>
+
+                )}
+
+                {/* Arquivos */}
+                <div id="fileList-container" className={`flex overflow-y-auto px-8 justify-center ${descriptionMap(SUBSESSION_NAME ?? '') ? 'w-1/2' : 'w-auto'}`}>
+                    <FileList files={files} />
+                </div>
+
+            </div>
+
+
         </div>
     );
 }
