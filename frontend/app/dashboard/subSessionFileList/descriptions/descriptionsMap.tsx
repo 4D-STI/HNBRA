529a4fb0bd58ccb43ce861947d6386d619b0530d
devHB <devhb@outlook.com.br>
2025-02-26 18:32:10 -0300

    FEAT: cria componente para mapear componentes de descrição da sub sessão.

---
+++ frontend/app/dashboard/subSessionFileList/descriptions/descriptionsMap.tsx
@@ -0,0 +1,24 @@
+import SFH_Description from './SFH_Description'
+import PlanoDiaPDDescription from './PlanoDiaPDDescription'
+import SEDIME_Description from './SEDIME_Description'
+import PME_Description from './PME_Description'
+
+export const descriptionMap = (title: string) => {
+    switch (title) {
+        case 'SERVICO_DE_FARMACIA_HOSPITALAR':
+            return <SFH_Description/>
+            break
+        case 'PLANO DIA - PD':
+            return <PlanoDiaPDDescription/>
+        case 'SEDIME':
+            return <SEDIME_Description/>
+        case 'PROGRAMA DE MEDICAMENTOS ESPECIAIS':
+            return <PME_Description/>
+        // case 'ORGANOGRAMA':
+        //     return true
+        default: return null
+    }
+    
+}
+
+export default descriptionMap
