9754576ff55eb7360b3794048eb5014b749c406e
devHB <devhb@outlook.com.br>
2025-03-25 10:17:49 -0300

    REFAT: remove (comenta) componente <Separator/>

--- frontend/app/dashboard/userManager/page.tsx
+++ frontend/app/dashboard/userManager/page.tsx
@@ -1,10 +1,10 @@
-import { Separator } from "@/components/ui/separator"
+// import { Separator } from "@/components/ui/separator"
 import ListUsers from "@/app/dashboard/userManager/listUsers"
 
 export default function AdminUserPage() {
   return (
     <div className="space-y-6">
-      <Separator />
+      {/* <Separator /> */}
       <ListUsers />
     </div>
   )
