78a22caeb6396d7a9e74bdf5532aa05cf8333b44
devHB <devhb@outlook.com.br>
2025-03-25 10:17:45 -0300

    REFAT: remove (comenta) componente <Separator/>

--- frontend/app/dashboard/filesManagement/page.tsx
+++ frontend/app/dashboard/filesManagement/page.tsx
@@ -1,10 +1,10 @@
-import { Separator } from "@/components/ui/separator"
+// import { Separator } from "@/components/ui/separator"
 import ListFiles from "@/app/dashboard/filesManagement/listFiles"
 
 export default function AdminUserPage() {
   return (
-    <div id="filesManagement-page-container" className="space-y-6">
-      <Separator />
+    <div id="filesManagement-page-container" className="space-y-6 mt-10">
+      {/* <Separator /> */}
       <ListFiles />
     </div>
   )
