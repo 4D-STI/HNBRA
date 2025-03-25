1731a79a4509c3014c356346374d675249988c45
devHB <devhb@outlook.com.br>
2025-03-25 18:35:15 -0300

    REFAT: comenta componentes <Separator/>

--- frontend/app/dashboard/filesManagement/page.tsx
+++ frontend/app/dashboard/filesManagement/page.tsx
@@ -1,10 +1,10 @@
-import { Separator } from "@/components/ui/separator"
+// import { Separator } from "@/components/ui/separator"
 import ListFiles from "@/app/dashboard/filesManagement/listFiles"
 
 export default function AdminUserPage() {
   return (
     <div id="filesManagement-page-container" className="space-y-6">
-      <Separator />
+      {/* <Separator /> */}
       <ListFiles />
     </div>
   )
