cdc8ffdbb512fd6af87f4c2ee1e2b4ed6db8ff94
devHB <devhb@outlook.com.br>
2025-03-28 10:32:32 -0300

    REFAT: atualiza a importação da interface "IScheduleItem" no componente <ScheduleItemContent/>.

--- frontend/app/dashboard/resourceManagement/components/schedule/ScheduleItemContent.tsx
+++ frontend/app/dashboard/resourceManagement/components/schedule/ScheduleItemContent.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import ScheduleItemTable from './ScheduleItemTable';
-import { IScheduleItem } from './schedule_interface/scheduleInterface'
+import { IScheduleItem } from '../../../../contracts/IScheduleInterface'
 
 interface IscheduleItemContent {
     data: IScheduleItem[]
